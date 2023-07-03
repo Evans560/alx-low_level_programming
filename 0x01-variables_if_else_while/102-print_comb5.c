8ebfa00830e9cd5419dc7d22daa71d500a97d120
Konkra1 <oevans536@gmailcom>
2023-07-03 02:26:47 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,9 +10,7 @@ int num1, num2;
 
 for (num1 = 0; num1 <= 99; num1++)
 {
-for (num2 = 0; num2 <= 99; num2++)
-{
-if (num1 <= num2)
+for (num2 = num1; num2 <= 99; num2++)
 {
 putchar(num1 / 10 + '0');
 putchar(num1 % 10 + '0');
@@ -21,10 +19,8 @@ putchar(num2 / 10 + '0');
 putchar(num2 % 10 + '0');
 
 if (num1 != 99 || num2 != 99)
-{
 putchar(',');
 putchar(' ');
-}
 }
 }
 }
@@ -33,3 +29,4 @@ putchar('\n');
 
 return (0);
 }
+
