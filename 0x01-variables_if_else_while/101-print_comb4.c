cd156fb47fba70362952aec3e587f405f46bdd31
Konkra1 <oevans536@gmailcom>
2023-07-03 01:54:04 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,24 +8,23 @@ int main(void)
 {
 int i, j, k, l;
 
-for (i = 0; i <= 9; i++)
+for (i = 0; i < 10; i++)
 {
-for (j = i + 1; j <= 9; j++)
+for (j = i + 1; j < 10; j++)
 {
-for (k = j + 1; k <= 9; k++)
-			{
-for (l = k + 1; l <= 9; l++)
-				{
+for (k = j + 1; k < 10; k++)
+{
+for (l = k + 1; l < 10; l++)
+{
 putchar(i + '0');
 putchar(j + '0');
+putchar(',');
+putchar(' ');
 putchar(k + '0');
 putchar(l + '0');
 
-if (i != 6 || j != 7 || k != 8 || l != 9)
-{
-putchar(',');
-putchar(' ');
-}
+if (!(i == 6 && j == 7 && k == 8 && l == 9))
+printf(", ");
 }
 }
 }
